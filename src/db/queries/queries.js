75153ebd2daa2b67125f7658aba27116bffb4425
Sergey Shliakhov <ss@applemint.eu>
2018-12-04 14:56:16 +0200

    Update queuries

--- src/db/queries/queries.js
+++ src/db/queries/queries.js
@@ -4,9 +4,8 @@ module.exports = {
     update_queue: `UPDATE queue SET queue.title = ?, queue.datetime = ?, queue.url = ?, queue.places = ?, queue.prestart = ? WHERE queue.id = ?`,
     delete_queue: `DELETE FROM queue WHERE queue.id = ?`,
     insert_place: `INSERT INTO place(queue_id) VALUES(?)`,
-    read_place_by_queue_id: `SELECT * FROM place WHERE place.queue_id = ?`,
-    update_place: `UPDATE place SET place.used = ?, place.number = ?, place.url = ?, place.proxy = ?, place.useragent = ?, place.cookies = ? WHERE place.id = ?`,
-    place_register_heartbeat: `UPDATE place SET place.heartbeat_at = ? WHERE place.id = ?`,
+    read_place_by_queue_id: `SELECT * FROM place WHERE place.queue_id = ? ORDER BY (number IS NULL) ASC, number ASC`,
+    update_place: `UPDATE place SET place.used = IFNULL(?, place.used), place.number = IFNULL(?, place.number), place.remote_id = IFNULL(?, place.remote_id), place.url = IFNULL(?, place.url), place.proxy = IFNULL(?, place.proxy), place.useragent = IFNULL(?, place.useragent), place.cookies = IFNULL(?, place.cookies), place.heartbeat_at = IFNULL(?, place.heartbeat_at) WHERE place.id = ?`,
     delete_place: `DELETE FROM place WHERE place.id = ?`,
     count_place: `SELECT count(id) as amount FROM place WHERE place.queue_id = ?`,
     //TODO rename
