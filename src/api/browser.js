a92d406a6626928bb3fe4547c59328d94e24f3a7
Sergey Shliakhov <ss@applemint.eu>
2018-12-12 13:44:41 +0200

    Replace setInterval to loop

--- src/api/browser.js
+++ src/api/browser.js
@@ -7,43 +7,46 @@ const queueDao = new QueueDao();
 const placeDao = new PlaceDao();
 const moment = require("moment")
 
-async function upBrowsers() {
-    try {
-        let queues = await queueDao.readEntities()
-        queues.forEach(async (queue) => {
-            if (moment().isBetween(moment(queue.datetime).subtract(queue.prestart, 'minutes'), moment(queue.datetime).add(process.env.BROWSER_SHUTDOWN_WAIT, 'minutes'))) {
-                let places = await placeDao.readEntitiesByQueueId(queue.id)
-                places.forEach((place) => {
-                    //TODO magic constant
-                    if (!place.url && (!place.heartbeat_at || moment(place.heartbeat_at).isBefore(moment().subtract(1, 'minutes')))) {
-                        var params = {
-                            DelaySeconds: 0,
-                            MessageBody: JSON.stringify({
-                                id: place.id,
-                                queue_id: queue.id,
-                                url: queue.url,
-                                cookies: place.cookies,
-                                callback: `${process.env.API_URL}/place/${place.id}`
-                            }),
-                            QueueUrl: process.env.AWS_SQS_URL
-                        };
+;(async () => {
+    while (true) {
+        try {
+            let queues = await queueDao.readEntities()
+            queues.forEach(async (queue) => {
+                if (moment().isBetween(moment(queue.datetime).subtract(queue.prestart, 'minutes'), moment(queue.datetime).add(process.env.BROWSER_SHUTDOWN_WAIT, 'minutes'))) {
+                    let places = await placeDao.readEntitiesByQueueId(queue.id)
+                    places.forEach((place) => {
+                        if (place.url !== undefined) {
+                            return;
+                        }
+                        //TODO magic constant
+                        if (!place.heartbeat_at || moment(place.heartbeat_at).isBefore(moment().subtract(1, 'minutes'))) {
+                            var params = {
+                                DelaySeconds: 0,
+                                MessageBody: JSON.stringify({
+                                    id: place.id,
+                                    queue_id: queue.id,
+                                    url: queue.url,
+                                    cookies: place.cookies,
+                                    callback: `${process.env.API_URL}/place/${place.id}`
+                                }),
+                                QueueUrl: process.env.AWS_SQS_URL
+                            };
 
-                        sqs.sendMessage(params, function (err, data) {
-                            if (err) {
-                                console.log("Error", err);
-                            } else {
-                                console.log("Success", data.MessageId);
-                            }
-                        });
-                    }
-                })
-            }
-        })
-    } catch (e) {
-        console.log(e.message)
+                            sqs.sendMessage(params, function (err, data) {
+                                if (err) {
+                                    console.log("Error", err);
+                                } else {
+                                    console.log("Success", data.MessageId);
+                                }
+                            });
+                        }
+                    })
+                }
+            })
+        } catch (e) {
+            console.log(e.message)
+        }
     }
-}
-
-setInterval(upBrowsers, 15000)
+})()
 
 
