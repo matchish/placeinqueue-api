24a80870119f12b402c8c73e87baf7b01b62ca5d
Sergey Shliakhov <ss@applemint.eu>
2018-12-12 14:12:45 +0200

    Wait betwen loop

--- src/api/browser.js
+++ src/api/browser.js
@@ -12,41 +12,47 @@ const moment = require("moment")
         try {
             let queues = await queueDao.readEntities()
             queues.forEach(async (queue) => {
+                console.log(moment().toISOString() ,moment(queue.datetime).subtract(queue.prestart, 'minutes'), moment(queue.datetime).add(process.env.BROWSER_SHUTDOWN_WAIT, 'minutes'))
                 if (moment().isBetween(moment(queue.datetime).subtract(queue.prestart, 'minutes'), moment(queue.datetime).add(process.env.BROWSER_SHUTDOWN_WAIT, 'minutes'))) {
                     let places = await placeDao.readEntitiesByQueueId(queue.id)
-                    places.forEach((place) => {
-                        if (place.url !== undefined) {
-                            return;
-                        }
-                        //TODO magic constant
-                        if (!place.heartbeat_at || moment(place.heartbeat_at).isBefore(moment().subtract(1, 'minutes'))) {
-                            console.log(place)
-                            var params = {
-                                DelaySeconds: 0,
-                                MessageBody: JSON.stringify({
-                                    id: place.id,
-                                    queue_id: queue.id,
-                                    url: queue.url,
-                                    cookies: place.cookies,
-                                    callback: `${process.env.API_URL}/place/${place.id}`
-                                }),
-                                QueueUrl: process.env.AWS_SQS_URL
-                            };
+                    await Promise.all(places.map((place) => {
+                        return new Promise((resolve) => {
+                            if (place.url !== undefined) {
+                                resolve()
+                            }
+                            //TODO magic constant
+                            if (!place.heartbeat_at || moment(place.heartbeat_at).isBefore(moment().subtract(1, 'minutes'))) {
+                                console.log(place)
+                                var params = {
+                                    DelaySeconds: 0,
+                                    MessageBody: JSON.stringify({
+                                        id: place.id,
+                                        queue_id: queue.id,
+                                        url: queue.url,
+                                        cookies: place.cookies,
+                                        callback: `${process.env.API_URL}/place/${place.id}`
+                                    }),
+                                    QueueUrl: process.env.AWS_SQS_URL
+                                };
 
-                            sqs.sendMessage(params, function (err, data) {
-                                if (err) {
-                                    console.log("Error", err);
-                                } else {
-                                    console.log("Success", data.MessageId);
-                                }
-                            });
-                        }
-                    })
+                                sqs.sendMessage(params, function (err, data) {
+                                    if (err) {
+                                        console.log("Error", err);
+                                    } else {
+                                        console.log("Success", data.MessageId);
+                                    }
+                                    resolve()
+                                });
+                            }
+
+                        })
+                    }))
                 }
             })
         } catch (e) {
             console.log(e.message)
         }
+        await new Promise((resolve) => setTimeout(resolve, 15000));
     }
 })()
 
